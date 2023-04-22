77cbbef29c88a5f8d67a625d693437f2cb2ba6d9
vatari <majestic13@gmail.com>
2023-04-22 18:36:22 +0300

    switched to external server API

--- Accordion/src/App.js
+++ Accordion/src/App.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
-import data from "./data";
+import React, { useState, useEffect } from "react";
+// import data from "./data"; // for usage from local data file
 import SingleQuestion from "./Question";
+
+const url = "http://b8e00a7b5ca8.sn.mynetname.net:3012/jsonstore/questions";
+
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [questions, setQuestions] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const questions = await response.json();
+      setQuestions(questions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <main>
       <div className="container">
         <h3>Questions and answers about Login</h3>
         <section className="info">
-          {questions.map((q) => {
+          {Object.values(questions).map((q) => {
             return <SingleQuestion key={q.id} {...q} />;
           })}
         </section>
