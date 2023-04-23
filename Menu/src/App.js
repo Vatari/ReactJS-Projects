5570c1aafe1541d586e5f9e09cbf438dbc690041
vatari <majestic13@gmail.com>
2023-04-23 18:59:24 +0300

    added some states

--- Menu/src/App.js
+++ Menu/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
+  const [menuItems, setmenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
   return <h2>menu project setup</h2>;
 }
 
