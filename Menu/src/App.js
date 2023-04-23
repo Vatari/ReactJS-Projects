8248069fdffd9d9216105315dcb7a638eae47f4b
vatari <majestic13@gmail.com>
2023-04-23 19:01:23 +0300

    added components to main App

--- Menu/src/App.js
+++ Menu/src/App.js
@@ -6,7 +6,18 @@ import items from "./data";
 function App() {
   const [menuItems, setmenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
-  return <h2>menu project setup</h2>;
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
