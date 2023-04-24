68943cafd71dd36b5785600983544841cc5baf33
unknown <majestic13@gmail.com>
2023-04-24 15:56:26 +0300

    switched to external api-server

--- Menu/src/App.js
+++ Menu/src/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const url = "http://b8e00a7b5ca8.sn.mynetname.net:3012/jsonstore/menu";
+
 function App() {
   const [menuItems, setmenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const items = await response.json();
+      setmenuItems(items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <main>
       <section className="menu section">
