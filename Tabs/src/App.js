8c5713750abc440386054cfdc2da7a17cd236786
vatari <majestic13@gmail.com>
2023-04-26 19:16:05 +0300

    switched to external url

--- Tabs/src/App.js
+++ Tabs/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "http://b8e00a7b5ca8.sn.mynetname.net:3012/jsonstore/tabs";
 function App() {
-  return <h2>tabs project setup</h2>
+  return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
