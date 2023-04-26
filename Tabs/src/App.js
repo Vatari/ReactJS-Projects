f3265d5b5429d021f4205e41c64feefad7f86703
vatari <majestic13@gmail.com>
2023-04-26 19:24:12 +0300

    switched to external url

--- Tabs/src/App.js
+++ Tabs/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = "http://b8e00a7b5ca8.sn.mynetname.net:3012/jsonstore/tabs";
 function App() {
   return <h2>tabs project setup</h2>;
