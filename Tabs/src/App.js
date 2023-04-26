d83fdaab1c9ebfd5e3996a29cbf1c647e3ad15c8
vatari <majestic13@gmail.com>
2023-04-26 19:29:39 +0300

    readme update

--- Tabs/src/App.js
+++ Tabs/src/App.js
@@ -3,6 +3,21 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 
 const url = "http://b8e00a7b5ca8.sn.mynetname.net:3012/jsonstore/tabs";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const jobs = await response.json();
+    setJobs(jobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <h2>tabs project setup</h2>;
 }
 
