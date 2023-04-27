c18476afe8d4c23b0fc63442c1f6de1145ec82df
vatari <majestic13@gmail.com>
2023-04-27 19:34:36 +0300

    switched to my api server

--- Slider/src/App.js
+++ Slider/src/App.js
@@ -1,8 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+
+const url = "http://b8e00a7b5ca8.sn.mynetname.net:3012/jsonstore/people";
+
 function App() {
+  const [people, setPeople] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPeople(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <h2>slider project setup</h2>;
 }
 
