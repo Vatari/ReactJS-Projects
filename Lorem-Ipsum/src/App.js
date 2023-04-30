ee33bcdd25e1b792c244df51d31dbf865a4b91ee
vatari <majestic13@gmail.com>
2023-04-30 07:11:50 +0300

    completed html skeleton

--- Lorem-Ipsum/src/App.js
+++ Lorem-Ipsum/src/App.js
@@ -11,7 +11,22 @@ function App() {
   return (
     <section className="section-center">
       <h3>Lorem Ipsum Generator</h3>
-      <form className="lorem-form" onSubmit={handleSubmit}></form>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate paragraphs
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>asdasd</p>
+      </article>
     </section>
   );
 }
