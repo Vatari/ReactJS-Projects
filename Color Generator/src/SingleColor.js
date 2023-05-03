ddec4e2822d541f71fa0e9f3817c3b97ac320823
vatari <majestic13@gmail.com>
2023-05-03 16:28:45 +0300

    added functionality to copy hex value in clipboard

--- Color Generator/src/SingleColor.js
+++ Color Generator/src/SingleColor.js
@@ -10,9 +10,14 @@ const SingleColor = ({ rgb, weight, index }) => {
     <article
       className={`color:${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
     </article>
   );
 };
