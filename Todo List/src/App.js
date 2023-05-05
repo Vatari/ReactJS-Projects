69cd8cca04c665f07d832066e4fabbca64395cdc
vatari <majestic13@gmail.com>
2023-05-05 14:04:52 +0300

    completed main structure

--- Todo List/src/App.js
+++ Todo List/src/App.js
@@ -16,6 +16,19 @@ function App() {
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert />}
+        <h3>To Do List</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
       </form>
       <div className="grocery-container">
         <List />
