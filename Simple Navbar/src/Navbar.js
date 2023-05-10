d47fc9fcc08b8e4965b9760e7e41545ca2db05ae
vatari <majestic13@gmail.com>
2023-05-10 08:54:48 +0300

    added basic structure of Navbar component

--- Simple Navbar/src/Navbar.js
+++ Simple Navbar/src/Navbar.js
@@ -13,7 +13,22 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container"></div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+          </ul>
+        </div>
         <ul className="social-icons"></ul>
       </div>
     </nav>
