3239feecf57286b53fddad4258c4a224b55c8a52
vatari <majestic13@gmail.com>
2023-05-12 08:06:59 +0300

    swithced to useRef for dynamic change of height

--- Simple Navbar/src/Navbar.js
+++ Simple Navbar/src/Navbar.js
@@ -5,6 +5,8 @@ import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
   return (
     <nav>
       <div className="nav-center">
@@ -17,12 +19,8 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
