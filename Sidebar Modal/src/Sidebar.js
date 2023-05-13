67c2d386cdd73888cb0c1f28e93a23875d7547e9
vatari <majestic13@gmail.com>
2023-05-13 08:37:27 +0300

    project completed

--- Sidebar Modal/src/Sidebar.js
+++ Sidebar Modal/src/Sidebar.js
@@ -5,11 +5,12 @@ import { social, links } from "./data";
 import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <aside className={`sidebar show-sidebar`}>
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
       <div className="sidebar-header">
         <img src={logo} className="logo" alt="logo" />
-        <button className="close-btn">
+        <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
