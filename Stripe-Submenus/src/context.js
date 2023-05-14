a6d0a3190aa9c661dfaf3087712cc53f6fd007f4
vatari <majestic13@gmail.com>
2023-05-14 08:34:27 +0300

    added global context

--- Stripe-Submenus/src/context.js
+++ Stripe-Submenus/src/context.js
@@ -1,2 +1,39 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = (children) => {
+  const [isSidebarOpen, setIsSideBarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSideBarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSideBarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
