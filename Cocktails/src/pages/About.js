738e231ab3daa0d5a971e6898872d12c91cfb1ba
unknown <majestic13@gmail.com>
2023-05-29 19:32:55 +0300

    completed About page

--- Cocktails/src/pages/About.js
+++ Cocktails/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        labore ut architecto tenetur aperiam dignissimos voluptas hic autem non
+        porro illo iure optio tempora iusto necessitatibus harum corporis,
+        assumenda incidunt?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
