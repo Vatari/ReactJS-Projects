41ae5bb071264803650db84e98998f844af444af
vatari <majestic13@gmail.com>
2023-06-01 08:35:01 +0300

    fetching data details

--- Cocktails/src/pages/SingleCocktail.js
+++ Cocktails/src/pages/SingleCocktail.js
@@ -5,6 +5,57 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
   const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const res = await fetch(`${url}${id}`);
+        const data = await res.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrink: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          setCocktail(newCocktail);
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    }
+    getCocktail();
+  }, [id]);
   return (
     <div>
       <h2>{id}</h2>
