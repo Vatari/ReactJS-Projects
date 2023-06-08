7ef922060f86cda2bbd411b09c62b185bb713907
vatari <majestic13@gmail.com>
2023-06-08 08:20:15 +0300

    additional logic

--- Pagination Example/src/utils.js
+++ Pagination Example/src/utils.js
@@ -1,7 +1,9 @@
 const paginate = (followers) => {
   const itemsPerPage = 9;
   const pages = Math.ceil(followers.length / itemsPerPage);
-  const newFollowers = Array.from({ length: pages });
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+  });
 };
 
 export default paginate;
