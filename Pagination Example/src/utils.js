763d54034b13ba1d0a385f2223ea6b04806ee75d
vatari <majestic13@gmail.com>
2023-06-08 08:24:54 +0300

    completed pagination function

--- Pagination Example/src/utils.js
+++ Pagination Example/src/utils.js
@@ -3,7 +3,9 @@ const paginate = (followers) => {
   const pages = Math.ceil(followers.length / itemsPerPage);
   const newFollowers = Array.from({ length: pages }, (_, index) => {
     const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
   });
+  return newFollowers;
 };
 
 export default paginate;
