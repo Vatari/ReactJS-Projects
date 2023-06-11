b32dfeb68fa78c25dcec42c7c08b3a56777341ac
vatari <majestic13@gmail.com>
2023-06-11 08:42:22 +0300

    moved API key to env variable

--- Photos/src/App.js
+++ Photos/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
@@ -11,7 +11,7 @@ function App() {
   const fetchPhotos = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=BwEo4txqX7VeK4WOcuyblsfoaYGUc3kyLKNmIjAcaf8`;
+    url = `${mainUrl}?client_id=${clientID}`;
     try {
       const res = await fetch(url);
       const data = await res.json();
