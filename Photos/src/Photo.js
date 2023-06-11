44f3f9432ec2a7c726b04347d9305ebcc4526743
vatari <majestic13@gmail.com>
2023-06-11 09:03:13 +0300

    additional properties added to Photo component

--- Photos/src/Photo.js
+++ Photos/src/Photo.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const Photo = ({ urls: { regular }, alt_desc }) => {
+const Photo = ({
+  urls: { regular },
+  alt_desc,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
   return (
     <article className="photo">
       <img src={regular} alt={alt_desc} />
+      <div className="photo-info"></div>
     </article>
   );
 };
