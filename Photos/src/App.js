f4384f6b156e5902ce9da45537da147cc9c558df
vatari <majestic13@gmail.com>
2023-06-13 08:16:45 +0300

    additional conditions in fetch

--- Photos/src/App.js
+++ Photos/src/App.js
@@ -16,10 +16,11 @@ function App() {
     let url;
     const urlPage = `&page=${page}`;
     const urlQuery = `&query=${query}`;
-    url = `${mainUrl}${clientID}${urlPage}`;
 
     if (query) {
       url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
     }
     try {
       const res = await fetch(url);
