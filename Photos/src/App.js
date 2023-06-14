8850ccc5a54c000bfb191d673d5ddceed90678b6
vatari <majestic13@gmail.com>
2023-06-14 08:20:01 +0300

    refactoring to fix some bugs

--- Photos/src/App.js
+++ Photos/src/App.js
@@ -8,7 +8,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
 
   const fetchPhotos = async () => {
@@ -44,21 +44,6 @@ function App() {
     fetchPhotos();
     // eslint-disable-next-line
   }, [page]);
-
-  useEffect(() => {
-    const event = window.addEventListener("scroll", () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => {
-          return oldPage + 1;
-        });
-      }
-    });
-    return () => window.removeEventListener("scroll", event);
-    // eslint-disable-next-line
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
