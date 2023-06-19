d20c2bb3be62c6b91a0beb65f179deaa1eeb3a8d
unknown <majestic13@gmail.com>
2023-06-19 18:52:30 +0300

    basic JSX structure for Movies component

--- Movies Database/src/Movies.js
+++ Movies Database/src/Movies.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  return <h2>movies component</h2>
-}
+  const { movies, isLoading } = useGlobalContext();
 
-export default Movies
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return <h2>movies component</h2>;
+};
+
+export default Movies;
