7b7749d0fb8726c94d8667547176474007fc62ee
unknown <majestic13@gmail.com>
2023-06-20 08:18:10 +0300

    basic JSX structure for Search component

--- Movies Database/src/SearchForm.js
+++ Movies Database/src/SearchForm.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  return <h2>search component</h2>
-}
+  const { query, setQuery } = useGlobalContext();
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>Search movies</h2>
+    </form>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
