7fd08de4a33d3cf83a14f7866947975ca4ee89c3
unknown <majestic13@gmail.com>
2023-06-20 08:20:51 +0300

    added error handling for search form

--- Movies Database/src/SearchForm.js
+++ Movies Database/src/SearchForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  const { query, setQuery } = useGlobalContext();
+  const { query, setQuery, error } = useGlobalContext();
   return (
     <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>Search movies</h2>
@@ -11,6 +11,7 @@ const SearchForm = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
+      {error.show && <div className="error">{error.msg}</div>}
     </form>
   );
 };
