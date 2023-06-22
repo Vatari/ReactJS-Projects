c39f41c7966420ecee4e06787087a2d74f5d326d
vatari <majestic13@gmail.com>
2023-06-22 08:11:25 +0300

    refactoringn

--- Movies Database/src/useFetch.js
+++ Movies Database/src/useFetch.js
@@ -1,8 +1,31 @@
 import React, { useState, useContext, useEffect } from "react";
 const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
-const useFetch = () => {
-  return <div></div>;
+const useFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}${urlParams}`);
+  }, [urlParams]);
+
+  return {};
 };
 
 export default useFetch;
