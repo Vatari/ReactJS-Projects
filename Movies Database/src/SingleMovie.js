3bb33254858b987354f6bff0144d8c045ca7c586
vatari <majestic13@gmail.com>
2023-06-22 08:21:52 +0300

    fixed API key request

--- Movies Database/src/SingleMovie.js
+++ Movies Database/src/SingleMovie.js
@@ -1,28 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { API_ENDPOINT } from "./context";
+import { API_ENDPOINT } from "./useFetch";
+import useFetch from "./useFetch";
 
 const SingleMovie = () => {
   const { id } = useParams();
-  const [movie, SetMovie] = useState({});
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: "" });
-
-  const fetchMovie = async (url) => {
-    const res = await fetch(url);
-    const data = await res.json();
-    if (data.Response === "False") {
-      setError({ show: true, msg: data.Error });
-      setLoading(false);
-    } else {
-      SetMovie(data);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`);
-  }, [id]);
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
 
   if (isLoading) {
     return <div className="loading"></div>;
