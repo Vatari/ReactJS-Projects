374ab8585954f3eba2ec980f2b9749b065c8abdd
vatari <majestic13@gmail.com>
2023-06-22 08:09:25 +0300

    refactoring fetch function

---
+++ Movies Database/src/useFetch.js
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect } from "react";
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+
+const useFetch = () => {
+  return <div></div>;
+};
+
+export default useFetch;
