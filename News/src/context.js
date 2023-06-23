e309a4693ebab379d25713f0149ece1cb1d98ae1
vatari <majestic13@gmail.com>
2023-06-23 08:39:42 +0300

    general structure for reducer

--- News/src/context.js
+++ News/src/context.js
@@ -11,7 +11,9 @@ import reducer from "./reducer";
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
 
-const initialState = {};
+const initialState = {
+  isLoading: true,
+};
 
 const AppContext = React.createContext();
 
@@ -21,7 +23,13 @@ const AppProvider = ({ children }) => {
   const fetchStories = async (url) => {
     dispatch({ type: "SET_LOADING" });
   };
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+
+  useEffect(() => {
+    fetchStories();
+  }, []);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
