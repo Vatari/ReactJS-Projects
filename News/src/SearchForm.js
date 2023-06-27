47729b3db3887c96629c66488836039e78c8b51e
unknown <majestic13@gmail.com>
2023-06-27 08:13:21 +0300

    added controlled input for search form

--- News/src/SearchForm.js
+++ News/src/SearchForm.js
@@ -4,8 +4,14 @@ import { useGlobalContext } from "./context";
 const SearchForm = () => {
   const { query, handleSearch } = useGlobalContext();
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>Search news</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
     </form>
   );
 };
