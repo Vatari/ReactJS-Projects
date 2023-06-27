faec3740ce055774c9d4683dd1974bf0ebcb34ea
unknown <majestic13@gmail.com>
2023-06-27 08:15:03 +0300

    added query to reducer function

--- News/src/reducer.js
+++ News/src/reducer.js
@@ -22,6 +22,8 @@ const reducer = (state, action) => {
         ...state,
         hits: state.hits.filter((story) => story.objectID !== action.payload),
       };
+    case HANDLE_SEARCH:
+      return { ...state, query: action.payload };
     default:
       throw new Error(`No matchin "${action.type}" action type`);
   }
