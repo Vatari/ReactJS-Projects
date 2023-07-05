b0a7ca910766c6ac5b034d6506a1b6ec88ccb11d
vatari <majestic13@gmail.com>
2023-07-05 08:21:21 +0300

    completed submit functionality for Quiz setup form

--- Quiz System/src/context.js
+++ Quiz System/src/context.js
@@ -86,8 +86,8 @@ const AppProvider = ({ children }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const { amount, category, difficulty } = quiz;
-
-    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=miltiple`;
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+    fetchQuestions(url);
   };
 
   return (
