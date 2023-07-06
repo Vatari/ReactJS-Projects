5ec765963fe5396787ebb870f37c85868614ecab
vatari <majestic13@gmail.com>
2023-07-06 08:39:14 +0300

    randomizing correct answer

--- Quiz System/src/App.js
+++ Quiz System/src/App.js
@@ -25,6 +25,12 @@ function App() {
 
   let answers = [...incorrect_answers];
   const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
 
   return (
     <main>
